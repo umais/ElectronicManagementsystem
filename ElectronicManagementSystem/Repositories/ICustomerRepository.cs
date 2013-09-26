8a952f6251883c3fdfaf9e04ea2ebfdc647935e7
Umais A Siddiqui <umais20@yahoo.com>
2013-09-26 15:24:35 -0400

    Added Entity Framework and POCO Classes
    Added the EF Classes and also used the T3 Template to generate POCO
    classes. Also created a separate folder for the Repository Patten.

---
+++ ElectronicManagementSystem/Repositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectronicManagementSystem.BusinessObjects;
+namespace ElectronicManagementSystem.Repositories
+{
+    interface ICustomerRepository
+    {
+
+        bool Insert(Customer c);
+        bool Update(Customer UpdatedCustomer);
+        bool DeleteCustomer(int id);
+        Customer getCustomer(int id);
+        List<Customer> getCustomers();
+    }
+}
