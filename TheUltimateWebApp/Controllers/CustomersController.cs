a3585355ffec200a603fa747f7869c2864fc7f8f
Umais Siddiqui <umais20@yahoo.com>
2013-09-26 20:07:23 -0400

    web api

---
+++ TheUltimateWebApp/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ElectronicManagementSystem.BusinessObjects;
+
+namespace TheUltimateWebApp.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        public Customer getCustomer(int id)
+        {
+            Customer c = new Customer { FirstName = "Amir", LastName = "Saleem" };
+
+            return c;
+        }
+
+        public void postCustomer([FromBody] Customer c)
+        {
+            Customer newcustomer = c;
+
+
+        }
+        
+    }
+}
